c8a1e3949304927a35a5214eb54d6b2d1a877e47
masaifeng <65411588@qq.com>
2020-07-01 15:08:21 +0800

    初始化完成了

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Field, Search } from 'vant'
+import 'amfe-flexible'
+Vue.use(Button)
+Vue.use(Field)
+Vue.use(Search)
 
 Vue.config.productionTip = false
 
